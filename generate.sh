c8ddef0c862717dec66aa689755093b59e3e5d13
Makopoppo <bokemako@gmail.com>
2013-06-22 21:14:40 +0900

    minor description change, parameterize OUTPUT_DIR

--- generate.sh
+++ generate.sh
@@ -1,10 +1,12 @@
+OUTPUT_DIR=${CONVERTER_OUTPUT_DIR:-outputs}
+
 rm lsl2dfg/LSL2dfg.py
 rm kwdb.xml
 
 curl https://kwdb.googlecode.com/hg/lsl2dfg/LSL2dfg.py -o lsl2dfg/LSL2dfg.py
 curl https://kwdb.googlecode.com/hg/database/kwdb.xml -o kwdb.xml
 
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl -i inputs/LSL.sublime-completions.in -o outputs/LSL.sublime-completions -y
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g os -i inputs/OSSL.sublime-completions.in -o outputs/OSSL.sublime-completions -y
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -i inputs/convertkeywords.pl.in | perl > outputs/LSL.tmLanguage
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -i inputs/convertkeywords.pl.in | perl > outputs/OSSL.tmLanguage
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl -i inputs/LSL.sublime-completions.in -o "$OUTPUT_DIR/LSL.sublime-completions" -y
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g os -i inputs/OSSL.sublime-completions.in -o "$OUTPUT_DIR/OSSL.sublime-completions" -y
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/LSL.tmLanguage"
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/OSSL.tmLanguage"
