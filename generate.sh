1e8be37fe7fd0367481f9174bd9076f927ff0cf9
Makopoppo <bokemako@gmail.com>
2013-07-13 16:25:31 +0900

    fixed typo

--- generate.sh
+++ generate.sh
@@ -8,5 +8,5 @@ curl https://kwdb.googlecode.com/hg/database/kwdb.xml -o kwdb.xml
 
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl -t LSL -i inputs/LSL.sublime-completions.in -o "$OUTPUT_DIR/LSL.sublime-completions" -y
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g os -t OSSL -i inputs/OSSL.sublime-completions.in -o "$OUTPUT_DIR/OSSL.sublime-completions" -y
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/LSL.tmLanguage"
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/OSSL.tmLanguage"
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage"
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/ossl.tmLanguage"
