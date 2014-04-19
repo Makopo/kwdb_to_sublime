ef9f2ef4f1958620f970ddeb84b4b41f66fa3ba5
Makopoppo <bokemako@gmail.com>
2014-04-19 22:50:28 +0900

    Completion files 2.0

--- generate.sh
+++ generate.sh
@@ -1,13 +1,17 @@
-OUTPUT_DIR=${CONVERTER_OUTPUT_DIR:-outputs}
+export OUTPUT_DIR=${CONVERTER_OUTPUT_DIR:-outputs}
 
 rm lsl2dfg/LSL2dfg.py
 rm kwdb.xml
 
+# mkdir $OUTPUT_DIR/snippets/functions
+# mkdir $OUTPUT_DIR/snippets/constants
+# mkdir $OUTPUT_DIR/snippets/events
+
 curl https://kwdb.googlecode.com/hg/lsl2dfg/LSL2dfg.py -o lsl2dfg/LSL2dfg.py
 curl https://kwdb.googlecode.com/hg/database/kwdb.xml -o kwdb.xml
 
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl -t LSL -i inputs/LSL.sublime-completions.in -o "$OUTPUT_DIR/LSL.sublime-completions" -y
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g os -t OSSL -i inputs/OSSL.sublime-completions.in -o "$OUTPUT_DIR/OSSL.sublime-completions" -y
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl -t LSL -i inputs/makesnippets.pl.in | perl
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g os -t OSSL -i inputs/makesnippets.pl.in | perl
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage"
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/ossl.tmLanguage"
 
