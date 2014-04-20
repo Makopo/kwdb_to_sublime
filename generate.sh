c2ed12356dca655cae824b9bc01dbd945e19f5c0
Makopoppo <bokemako@gmail.com>
2014-04-20 10:36:27 +0900

    refactor

--- generate.sh
+++ generate.sh
@@ -3,15 +3,14 @@ export OUTPUT_DIR=${CONVERTER_OUTPUT_DIR:-outputs}
 rm lsl2dfg/LSL2dfg.py
 rm kwdb.xml
 
-# mkdir $OUTPUT_DIR/snippets/functions
-# mkdir $OUTPUT_DIR/snippets/constants
-# mkdir $OUTPUT_DIR/snippets/events
+mkdir -p $OUTPUT_DIR/snippets/functions
+mkdir -p $OUTPUT_DIR/snippets/constants
+mkdir -p $OUTPUT_DIR/snippets/events
 
 curl https://kwdb.googlecode.com/hg/lsl2dfg/LSL2dfg.py -o lsl2dfg/LSL2dfg.py
 curl https://kwdb.googlecode.com/hg/database/kwdb.xml -o kwdb.xml
 
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl -t LSL -i inputs/makesnippets.pl.in | perl
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g os -t OSSL -i inputs/makesnippets.pl.in | perl
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl,os -i inputs/makesnippets.pl.in | perl
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage"
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/ossl.tmLanguage"
 
