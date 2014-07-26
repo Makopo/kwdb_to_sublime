ba8ef9e66c761f055f4b86b2efa5d317b9582a7a
Makopoppo <bokemako@gmail.com>
2014-07-26 14:17:30 +0900

    Forgotten bug - module name typo

--- generate.sh
+++ generate.sh
@@ -12,7 +12,7 @@ python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_constants -g sl 
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_constants -g os -i inputs/OSSLConstants.sublime-completions.in -o $OUTPUT_DIR/OSSLConstants.sublime-completions
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_functions -g sl -i inputs/LSLFunctions.sublime-completions.in -o $OUTPUT_DIR/LSLFunctions.sublime-completions
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_functions -g os -i inputs/OSSLFunctions.sublime-completions.in -o $OUTPUT_DIR/OSSLFunctions.sublime-completions
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions -g sl,os -i inputs/makesnippets.pl.in | perl
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_events -g sl,os -i inputs/makesnippets.pl.in | perl
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage"
 python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/ossl.tmLanguage"
 
